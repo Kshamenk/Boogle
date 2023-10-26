8bd9421e5457e8f89ab826fb66a56573b08524aa
Kshamenk Benitez <122121634+Kshamenk@users.noreply.github.com>
2023-10-26 18:02:47 -0300

    Body and styled components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path='/search' element={<h1>SearchPage</h1>}/>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Home.css'
+import AppsIcon from '@mui/icons-material/Apps';
+import { Avatar, IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Search from '../Components/Search/Search';
 
 
 const Home = () => {
@@ -7,15 +11,23 @@ const Home = () => {
     <div  className='home'>
         <div className="home__header">
             <div className="home__headerLaeft">
-                <p>Sobre Google</p>
-                <p>Tienda</p>
+                <Link to='/about'>Sobre Google</Link>
+                <Link to='/store'>Tienda</Link>
             </div>
 
             <div className="home__headerRight">
-                <p>Gmail</p>
-                <p>Imagenes</p>
-                <p>🟰</p>
+                <Link to='/gmail'>Gmail</Link>
+                <Link to='/images'>Imagenes</Link>
+                <IconButton>
+                <AppsIcon/>
+                </IconButton>
+                <Avatar/>
             </div>
+        </div>
+        <div className="home__body">
+          {/* <img src="http://1000marcas.net/wp-content/uploads/2019/11/Google-Logo.png" alt="" /> */}
+          <img src="https://wepik.com/api/image/ai/9a76bdc2-00f9-4b20-9ea9-81a1648642e5?thumb=1" alt="" />
+          <Search />
         </div>
     </div>
   )
